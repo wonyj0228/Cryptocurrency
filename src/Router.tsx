ac558ef5641080fd2da6e1ae518bc6411596568e
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-09-30 16:57:11 +0900

    배포용 basename 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,17 +4,14 @@ import Coin from './routes/Coin';
 import Chart from './routes/Chart';
 
 function Router() {
-  return createBrowserRouter(
-    [
-      { path: '/', element: <Coins /> },
-      {
-        path: '/:coinId',
-        element: <Coin />,
-        children: [{ path: 'chart', element: <Chart /> }],
-      },
-    ],
-    { basename: '/Cryptocurrency' }
-  );
+  return createBrowserRouter([
+    { path: '/', element: <Coins /> },
+    {
+      path: '/:coinId',
+      element: <Coin />,
+      children: [{ path: 'chart', element: <Chart /> }],
+    },
+  ]);
 }
 
 export default Router;
